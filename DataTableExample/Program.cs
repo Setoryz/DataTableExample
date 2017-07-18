87ba97989246b4e72c90a70ece849cca79efca95
Odukoya Jesuseyitan <Setoryz@gmail.com>
2017-07-18 04:49:21 +0100

    Add project files.

---
+++ DataTableExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataTable dt = new DataTable("Test"))
+            {
+                //add columns
+                dt.Columns.Add("ID", typeof(int));
+                dt.PrimaryKey = new DataColumn[] {dt.Columns["ID"]};
+                dt.Columns.Add("Dosage", typeof(int));
+                dt.Columns.Add("Drug", typeof(string));
+                dt.Columns.Add("Patient", typeof(string));
+                dt.Columns.Add("Date", typeof(DateTime));
+
+                //add data to rows
+                dt.Rows.Add(1,1, "Indocin", "David", DateTime.Now);
+                dt.Rows.Add(2, 2, "Panadol", "Sam", DateTime.Now);
+                dt.Rows.Add(3, 3, "Amalar", "Victor", DateTime.Now);
+                dt.Rows.Add(4, 4, "Lumartem", "Ada", DateTime.Now);
+                dt.Rows.Add(5, 5, "Cofta", "Emmanuel", DateTime.Now);
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Console.WriteLine("ID={4}, Dosage - {0}, Drug - {1}, Patient - {2}, Date - {3}", dr["Dosage"], dr["Drug"], dr["Patient"], dr["Date"], dr["ID"]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
